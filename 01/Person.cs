73443f05cf99c6f729c92b569a9ab54de095ab5e
2i1i2 <nitinkumar42045@gmail.com>
2023-07-07 07:31:06 +0530

    classes for essential

---
+++ 01/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _01
+{
+    public class Person
+    {
+        public string PersonName { get; set; }
+
+        public Person() { }
+
+        public Person(string name)
+        {
+            PersonName = name;
+        }
+        // method
+        public void PersonNameMethod()
+        {
+            Console.WriteLine($"Person name is: {PersonName}");
+        }
+    }
+}
---
+++ 01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.PersonName = "Sukhveer singh";
+
+            Person person2 = new Person("John Sinha");
+            
+            // calling method
+            person.PersonNameMethod();
+
+            Console.WriteLine(person2.PersonName);
+            Console.ReadKey();
+        }
+    }
+}

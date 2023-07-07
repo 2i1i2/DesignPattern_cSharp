fab382989f1ea0a777b3ecbf5789d8bd88cf84d8
2i1i2 <nitinkumar42045@gmail.com>
2023-07-07 07:13:23 +0530

    first commit

---
+++ Essential/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Essential features
+             * Classes
+             * Interfaces
+             * Encapsulation
+             * Abstraction
+             * Inheritance
+             * Polymorphism
+             * UML
+             */
+
+            // Instance 
+            User user = new User();
+
+            User user1 = new User(name: "Nitin Kumar Gautam"); // parametrized constructor
+
+            user.Name = "Nitin";
+
+            // calling method
+            user.sayHelllo();
+
+            Console.WriteLine(user.Name);
+
+
+            Console.WriteLine($"{user1}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Essential/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Essential
+{
+    public class User
+    {
+        // fields
+        public string Name;
+
+        
+        // getter and setter can use 3 types
+        // 1.
+
+         public  string _name { get; set; }
+
+
+        // 2nd
+        public string address;
+        public string myAddress
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        // 3rd
+        //getter 
+        public string getName()
+        {
+            return this.Name;
+        }
+        // setter
+        public void setName(string value)
+        {
+            this.Name = value;
+        }
+
+        // empty constructor
+        public User()
+        {
+
+        }
+
+        // paramatrized constructor
+        public User(string name)
+        {
+            this.Name = name;
+        }
+
+
+        // method
+        public void sayHelllo()
+        {
+            Console.WriteLine($"Hi my name is {Name}");
+        }
+
+    }
+}
